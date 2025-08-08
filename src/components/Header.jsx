c3a0943176da600d4bd22174edf14ee842c7acd5
Daneshwar Vema <161022737+DaneshVerma@users.noreply.github.com>
2025-08-08 16:47:27 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import SearchBar from "../SearchBar";
 const Header = () => {
   return (
     <>
-      <header className='sticky top-0 bg-white z-10 shadow-sm px-10'>
+      <header className='sticky top-0 bg-white z-10 shadow-sm md:px-10 px-2'>
         <div className='flex items-center justify-between gap-8 px-4 h-14'>
           <div className='flex'>
             <Link to={'/'} className='text-xl font-bold'>PlAYER UI</Link>

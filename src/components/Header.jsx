eacef4cf76cd1de6c53eda0e3b6f5d82ba64a8da
Daneshwar Vema <161022737+DaneshVerma@users.noreply.github.com>
2025-08-08 17:21:50 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <header className='sticky top-0 bg-white z-10 shadow-sm md:px-10 px-2'>
         <div className='flex items-center justify-between gap-8 px-4 h-14'>
           <div className='flex'>
-            <Link to={'/'} className='text-xl font-bold'>PlAYER UI</Link>
+            <Link to={'/'} className='text-xl font-bold'>PLAYER UI</Link>
           </div>
           <SearchBar />
           <div>
